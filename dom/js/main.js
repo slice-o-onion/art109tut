38d812044a9b51a1bdd3f350bf46e7e5c824efaa
slice-o-onion <slice.o.onion@gmail.com>
2024-04-10 03:44:57 -0700

    dom7made
    it hath been made!

---
+++ dom/js/main.js
@@ -0,0 +1,37 @@
+
+document.querySelector("#image-1").addEventListener("click", function(){
+    document.querySelector("#image-1").style.visibility = "hidden";
+
+})
+document.querySelector("#image-2").addEventListener("click", function(){
+    document.querySelector("#image-2").style.visibility = "hidden";
+    
+})
+document.querySelector("#image-3").addEventListener("click", function(){
+    document.querySelector("#image-3").style.visibility = "hidden";
+    
+})
+document.querySelector("#image-4").addEventListener("click", function(){
+    document.querySelector("#image-4").style.visibility = "hidden";
+    
+})
+document.querySelector("#image-5").addEventListener("click", function(){
+    document.querySelector("#image-5").style.visibility = "hidden";
+    
+})
+document.querySelector("#image-6").addEventListener("click", function(){
+    document.querySelector("#image-6").style.visibility = "hidden";
+    
+})
+document.querySelector("#image-7").addEventListener("click", function(){
+    document.querySelector("#image-7").style.visibility = "hidden";
+    
+})
+document.querySelector("#image-8").addEventListener("click", function(){
+    document.querySelector("#image-8").style.visibility = "hidden";
+    
+})
+document.querySelector("#image-9").addEventListener("click", function(){
+    document.querySelector("#image-9").style.visibility = "hidden";
+    
+})
